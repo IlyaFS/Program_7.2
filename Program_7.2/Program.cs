dc6a879832a08de8dbf1024b74acb0e0b2b5a889
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-04 16:22:05 +0300

    Добавьте файлы проекта.

---
+++ Program_7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+
+
+            CalcS(a, out S, out V);
+            Console.WriteLine();
+            Console.WriteLine("Площадь повержности куба: {0}", S);
+            Console.WriteLine("Объем куба: {0}", V);
+            Console.ReadKey();
+        }
+        static void CalcS(int a, out double S, out double V)
+        {
+            S = 6 * Math.Pow(a, 2);
+            V = Math.Pow(a, 3);
+        }
+
+    }
+}
